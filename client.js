02f953d71e1b855ef3b0a2beb74aee69d8bef93b
Ciprian Tomoiaga <ciprian.tomoiaga@gmail.com>
2016-11-01 16:48:24 +0100

    finalised Address

--- client.js
+++ client.js
@@ -1,75 +1,9 @@
 // Util.init_logging('debug');
 "use strict";
 
-var LE_NETWORK_SB_ADDR = 88;
+// constants
+var LE_NETWORK_SB_ADDR = 88; // default length of address buffer
 var HOST = "ws://127.0.0.1:43420"
-
-
-function Address(addrStr) {
-    // fields
-    this.time = this.depth = this.size = 0;
-    this.digits = [];
-    this.socket = null;
-
-    if (addrStr) {
-        let parts = addrStr.split('/');
-        if (parts.length != 4 || parts[0] || !parts[1] || !parts[2] || !parts[3])
-            throw("Invalid address");
-
-        this.time = parseInt(parts[1]);
-        this.size = parts[2].length;
-        for (let i = 0; i < this.size; ++i)
-            this.digits.push(parts[2][i]);
-        this.depth = parseInt(parts[3]);
-    }
-
-    this.toBytes = function() {
-        let bytes = [];
-        bytes = bytes.concat(num2bytes(this.size));
-        bytes = bytes.concat(num2bytes(this.time));
-        bytes = bytes.concat(num2bytes(this.depth));
-        bytes = bytes.concat(this.digits);
-        for (let i = bytes.length; i < LE_NETWORK_SB_ADDR; ++i)
-            bytes[i] = 0;
-        return bytes;
-    }
-
-    this.connect = function () {
-        if (this.socket == null) {
-            this.socket = new Websock();
-            this.socket.waithingAuth = true;
-            this.socket.on("message", this.connect);  // return here until auth
-            this.socket.on("close"  , this.close_conn);
-
-            this.socket.open(HOST, 'binary');
-            authenticate(this.socket);
-        }
-        else if (this.socket.waitingAuth) {
-            // it arrives here on first message
-            this.socket.waitingAuth = false;
-            // now we are ready to receive messages
-            this.socket.on("message", this.receive);
-        }
-        else
-            throw "Socket already connected";
-    }
-
-    this.receive = function () {
-        // TODO
-    }
-
-    this.close_conn = function () {
-        this.socket = null;
-    }
-
-
-    this.query = function() {
-        connect();
-        socket.send(this.toBytes());
-    }
-
-    return this;
-}
 
 function num2bytes(num) {
     let bytes = [];
@@ -80,63 +14,108 @@ function num2bytes(num) {
     return bytes;
 }
 
+function Address(addrStr) {
+    // keep reference to itself to be accessed from private functions
+    var self = this;
+
+    // fields
+    self.time = self.depth = self.size = 0;
+    self.digits = [];
+    let socket = null;
+    let num_messages = 0;
+    let data = [];
 
 
-function authenticate(socket) {
-    socket.send([2, 0, 0, 0]);
+    // construct from string
+    if (addrStr) {
+        let parts = addrStr.split('/');
+        if (parts.length != 4 || parts[0] || !parts[1] || !parts[2] || !parts[3])
+            throw("Invalid address");
 
-    // wait for the first reply to arrive
-    let num_tries = 10;
-    let poll = setInterval(function waitReply() {
-        if (socket.waitingAuth === true) {
-            if (num_tries > 0) {
-                num_tries--;
-                return;
-            }
-            clearInterval(poll);
-            throw "Timeout while authenticating socket";
-        }
-        clearInterval(poll);
-        let r = socket.rQshift32();
-        if ((r & 0x7F) === 2)
-            socket.auth = true;
-        else
-            throw "Failed to authenticate socket";
-    }, 100);
-}
-
-
-conn.on('open', function() {
-    console.log("Opened!");
-    authenticate(conn);
-});
-
-conn.on('message', function() {
-    if (conn.waitingAuth === true)
-        conn.waitingAuth = false;
-    else {
-
+        self.time = parseInt(parts[1]);
+        self.size = parts[2].length;
+        for (let i = 0; i < self.size; ++i)
+            self.digits.push(parts[2][i]);
+        self.depth = parseInt(parts[3]);
     }
 
-    // console.log("New message!");
-});
+    function toBytes() {
+        let bytes = [];
+        bytes = bytes.concat(num2bytes(self.size));
+        bytes = bytes.concat(num2bytes(self.time));
+        bytes = bytes.concat(num2bytes(self.depth));
+        bytes = bytes.concat(self.digits);
+        for (let i = bytes.length; i < LE_NETWORK_SB_ADDR; ++i)
+            bytes[i] = 0;
+        return bytes;
+    }
 
-conn.on('close', function(event) {
-    console.log("Closed! Cause: " + event.reason);
-});
+    function auth_and_send(message_buf) {
+        /* Callback for the first message on the socket.
+         * Checks it is correctly authenticating the socket
+         * Should not be called directly! Pairs with `connect`
+         */
+        console.log("Receiving auth");
+        // let r = new Int32Array(message_buf);  // 4 bytes
+        let r = new DataView(message_buf).getInt32(0, true);  // 4 bytes, littleEndian
+        if ((r & 0x7F) === 2) {
+            // auth succeeded, now we're ready to receive messages:
+            socket.on('message', receive);
+            socket.send(toBytes());
+        }
+        else
+            throw "Failed to authenticate socket";
+    }
 
-conn.on('error', function(msg) {
-    console.log("Error: ");
-    console.log(msg);
-});
+    function receive(message_buf) {
+        num_messages++;
+        let message = new Float64Array(message_buf);
+        for (let i = 0; i < message.length; ++i)
+            data.push(message);
+    }
+
+    function close_conn() {
+        socket = null;
+        console.log("received messages: " + num_messages);
+        console.log(data);
+    }
+
+
+    function connect() {
+        // Connects and authenticates this address' socket to the server
+        if (socket === null) {
+            socket = new Websock();
+            socket.on('open', function sendAuth() {
+                console.log("Sending auth");
+                socket.send([2, 0, 0, 0]);
+            });
+
+            // first message is processed to check authentication
+            socket.on("message", auth_and_send);
+            socket.on("close"  , close_conn);
+
+            socket.open(HOST, 'binary');
+        }
+        else
+            throw "Socket already connected";
+    }
+
+    self.query = function() {
+        connect();  // falls through auth -> send -> receive
+    }
+
+    return self;
+}
+
 
 // conn.open("ws://127.0.0.1:43427", "binary");
 // let addr = new Address("/950486422/122010001340232/1");
 
 // setTimeout(conn.send, 2000, addr.toBytes());
 
-function query(addr) {
-    addr = new Address(addr);
+function query(addr_str) {
+    let addr = new Address(addr_str);
+    addr.query();
 }
 
 // conn.close();
