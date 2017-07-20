a1fa283524bd7ac530dd61af84207f14256cfd48
Ciprian Tomoiaga <ciprian.tomoiaga@gmail.com>
2017-07-20 22:56:49 +0300

    cleanup of index; implement change of auto fill

--- src/client.js
+++ src/client.js
@@ -7,18 +7,20 @@ export default class Client {
   constructor(canvasID) {
     WebUtil.init_logging(); // use with link as http://....?logging=info
 
-    this.model = new Model(document.getElementById(canvasID), document.getElementById('autoFill').checked);
+    const autoFill = document.getElementById('autoFill');
+
+    this.model = new Model(document.getElementById(canvasID), autoFill.checked);
+    autoFill.addEventListener('change', (event) => { this.model.autoFill = event.target.checked; });
+
+
     this.gui = new GUI.GUI();
     // this.gui.add(this.model.controls, 'zoomSpeed'  , 0, 3);
     // this.gui.add(this.model.controls, 'rotateSpeed', 0, 3);
     // this.gui.add(this.model.controls, 'panSpeed'   , 0, 3);
-
-
-    // TODO: move in Model
   }
 }
 
 // for debugging, also export these:
 export { Model };
 export { Address } from './address';
-export Serial from './serial';
+export { default as Serial } from './serial';
